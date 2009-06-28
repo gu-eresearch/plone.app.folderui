108051da8516cbbf7bd60ba60ef3e900ff3ed72b
csenger <csenger@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2009-06-28 12:54:26 +0000

    move package sources into src/ to turn the checkout into a buildout while preventing problems with testrunner
    git-svn-id: http://svn.plone.org/svn/plone/plone.app.folderui/trunk@27699 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- setup.py
+++ setup.py
@@ -3,11 +3,16 @@ import os
 
 version = '0.01'
 
+def read(*rnames):
+    '''read files given a sequence of path segments'''
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+
 setup(name='plone.app.folderui',
       version=version,
       description="Folder user interface enhancements for Plone.",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=read('README.txt') + "\n" +
+                       read('docs', 'HISTORY.txt'),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
         "Framework :: Plone",
@@ -19,7 +24,8 @@ setup(name='plone.app.folderui',
       author_email='sdupton@gmail.com',
       url='http://dev.plone.org/plone/wiki/Gsoc2009/FolderUiImprovements',
       license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
+      packages=find_packages('src', exclude=['ez_setup']),
+      package_dir={'': 'src'},
       namespace_packages=['plone', 'plone.app'],
       include_package_data=True,
       zip_safe=False,
