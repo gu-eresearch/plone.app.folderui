e4657f149034a5db9a3018a7a2f0c5692c1d9a94
csenger <csenger@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2009-07-04 21:04:12 +0000

    test for vocabularies
    git-svn-id: http://svn.plone.org/svn/plone/plone.app.folderui/trunk@27819 3b4cdb85-528f-c531-b63d-5919d5b1aa08

---
+++ src/plone/app/folderui/tests/test_vocab.py
@@ -0,0 +1,95 @@
+import unittest
+from zope.interface import implements
+from plone.app.folderui.interfaces import ITitleLookup
+from zope.schema.interfaces import IVocabularyTokenized
+from plone.app.folderui.tests.base import BaseTestCase
+
+from plone.app.folderui.vocab import member_fullname, all_creators_vfactory, \
+     UniqueValuesFactory
+
+class MockCatalog(object):
+    def __init__(self, values=[]):
+        self.values = values
+        
+    def uniqueValuesFor(self, index):
+        return self.values
+
+class MockContext(object):
+
+    def __init__(self, catalog):
+        self.portal_catalog = catalog
+
+class MockTitlefn(object):
+    implements(ITitleLookup)
+    
+    def __call__(self, value, context):
+        return u'MockTitle'
+        
+class TestVocabularies(BaseTestCase):
+    ''' tests for BaseTestCase '''
+
+    def test_uvf(self):
+        '''
+        Test the UniqeValueFactory
+        '''
+        uvf = UniqueValuesFactory('_does_not_matter_here_')
+        # empty index
+        mock_context = MockContext(
+            MockCatalog( values=[] )
+            )
+        vocabulary = uvf(mock_context)
+        assert len(vocabulary) == 0
+        assert IVocabularyTokenized.providedBy(vocabulary)
+
+        # index with a value
+        mock_context = MockContext(
+            MockCatalog( values = ['value'] )
+            )
+        vocabulary = uvf(mock_context)
+        assert IVocabularyTokenized.providedBy(vocabulary)
+        assert len(vocabulary) == 1
+        assert 'value' in vocabulary
+
+    def test_uvf_titlefn(self):
+        uvf = UniqueValuesFactory('_does_not_matter_here_', titlefn = MockTitlefn())
+        # empty index
+        mock_context = MockContext(
+            MockCatalog( values=['value'] )
+            )
+        vocabulary = uvf(mock_context)
+        term = vocabulary.getTerm('value')
+        assert term.title == u'MockTitle'
+        
+    def test_fullname(self):
+        '''
+        give us the fullname or, if no fullname is saved, the username
+        '''
+        self.add_member('testuser', fullname='Test User')
+        fullname = member_fullname('testuser', self.portal)
+        assert fullname == 'Test User'
+
+        self.add_member('nameless', fullname='')
+        fullname = member_fullname('nameless', self.portal)
+        assert fullname == 'nameless'
+
+        self.add_member('none', fullname=None)
+        fullname = member_fullname('none', self.portal)
+        assert fullname == 'none'
+
+    def test_all_creators(self):
+        '''
+        all_creators gives us a SimpleVocabulary with all users that have
+        created content in the portal
+        '''
+        self.add_member('testuser', fullname='Test User')
+        self.add_content('testcontent', username='testuser')
+        self.add_member('second')
+        vocabulary = all_creators_vfactory(self.portal)
+        assert len(vocabulary) == 1
+        assert 'testuser' in vocabulary
+
+        
+def test_suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestVocabularies))
+    return suite
