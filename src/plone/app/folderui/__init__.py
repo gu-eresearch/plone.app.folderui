ac3e5b9906967421c3780dfee4d51af2983fbf5c
seanupton <seanupton@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2009-07-05 19:57:32 +0000

    (folderui) added skins fsdirview
    git-svn-id: http://svn.plone.org/svn/plone/plone.app.folderui/trunk@27831 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- src/plone/app/folderui/__init__.py
+++ src/plone/app/folderui/__init__.py
@@ -1,3 +1,6 @@
+from Products.CMFCore.DirectoryView import registerDirectory
 
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
+    registerDirectory('skins', globals())
+
