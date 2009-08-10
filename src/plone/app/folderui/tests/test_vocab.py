ced5715b9fc1d1890fe7f3f20632b0712ca4d411
seanupton <seanupton@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2009-08-10 04:20:58 +0000

    (folderui) vocab test now tests dynamic vocabulary factories, including utility lookup (now that registration of factories is in zcml).
    git-svn-id: http://svn.plone.org/svn/plone/plone.app.folderui/trunk@28607 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- src/plone/app/folderui/tests/test_vocab.py
+++ src/plone/app/folderui/tests/test_vocab.py
@@ -1,11 +1,17 @@
 import unittest
 from zope.interface import implements
-from plone.app.folderui.interfaces import ITitleLookup
-from zope.schema.interfaces import IVocabularyTokenized
-from plone.app.folderui.tests.base import BaseTestCase
+from zope.schema.interfaces import IVocabularyTokenized, IVocabularyFactory
 
+
+from plone.app.folderui.tests.base import BaseTestCase
+from plone.app.folderui.interfaces import ITitleLookup
 from plone.app.folderui.vocab import member_fullname, all_creators_vfactory, \
      UniqueValuesFactory
+
+from zope.component import queryUtility, getGlobalSiteManager
+from plone.app.folderui.defaults import FACETS
+from plone.app.folderui.interfaces import IFacetSettings
+
 
 class MockCatalog(object):
     def __init__(self, values=[]):
@@ -87,9 +93,23 @@ class TestVocabularies(BaseTestCase):
         vocabulary = all_creators_vfactory(self.portal)
         assert len(vocabulary) == 2
         assert 'testuser' in vocabulary
+    
+    def test_dynamic_terms(self):
+        self.add_samples()
+        facets = queryUtility(IFacetSettings)
+        for facet in facets.values():
+            if facet.query_vocabulary:
+                dynamic = queryUtility(IVocabularyFactory, name=facet.name)
+                assert dynamic is not None
+                assert IVocabularyFactory.providedBy(dynamic)
+                vocab = dynamic(self.portal)
+                hybrid = facet(self.portal) #may be superset including vocab
+                for t in vocab:
+                    assert t.value in [e.value for e in hybrid]
+                assert len(vocab) > 0 #this is true for sample content
 
-        
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTest(unittest.makeSuite(TestVocabularies))
     return suite
+
