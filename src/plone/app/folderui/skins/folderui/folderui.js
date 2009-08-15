bfacd0acd10af3c5fd73be5b3c0fc111554150e5
seanupton <seanupton@3b4cdb85-528f-c531-b63d-5919d5b1aa08>
2009-08-15 22:10:24 +0000

    (folderui) some minor javascript enhancements for checkbox UI on multiset facet selection.
    git-svn-id: http://svn.plone.org/svn/plone/plone.app.folderui/trunk@28958 3b4cdb85-528f-c531-b63d-5919d5b1aa08

--- src/plone/app/folderui/skins/folderui/folderui.js
+++ src/plone/app/folderui/skins/folderui/folderui.js
@@ -1,21 +1,41 @@
+folderui = new Object(); //namespace
 
-function toggle_facet() {
+folderui.toggle_facet = function() {
     context = jQuery(this).parent();
     jQuery('.options', context).toggle();
 }
 
-function toggle_morefilters() {
+folderui.toggle_morefilters = function() {
     context = jQuery(this).parent();
     jQuery('ul.facetmenu', context).toggle();
 }
 
-
-function facet_helper() {
-    jQuery('div.searchfilter h3').bind('click', toggle_facet);
-    jQuery('div.morefilters h4').bind('click', toggle_morefilters);
-    jQuery('div.morefilters ul.facetmenu').hide(); //default:hide, click expands
-    /* jQuery('div.searchfilter ul').toggle(); */
+folderui._show_conjunction = function(context) {
+    jQuery('div.conjunction_choice', context).show();
 }
 
-jQuery(document).ready(facet_helper);
+folderui.show_conjunction = function() {
+    context = jQuery(this).parent().parent().parent(); //div.searchfilter
+    folderui._show_conjunction(context);
+}
+
+
+folderui.facet_helper = function() {
+    jQuery('div.searchfilter h3').bind('click', folderui.toggle_facet);
+    jQuery('div.morefilters h4').bind('click', folderui.toggle_morefilters);
+    jQuery("div.searchfilter ul.facetmenu input[type='checkbox']").bind('click', folderui.show_conjunction);
+    jQuery('div.morefilters ul.facetmenu').hide(); //default:hide, click expands
+    jQuery('div.searchfilter div.conjunction_choice').hide(); //hide until checkbox click
+    var facetboxes = jQuery('div.searchfilter');
+    for (var i=0; i<facetboxes.length; i++) {
+        var box = jQuery(facetboxes[i]);
+        var checkboxes = jQuery(":checked",  jQuery("ul", box));
+        for (var j=0; j<checkboxes.length; j++) {
+            var checkbox = checkboxes[j];
+            folderui._show_conjunction(jQuery(checkbox).parent().parent().parent());
+        }
+    }
+}
+
+jQuery(document).ready(folderui.facet_helper);
 
