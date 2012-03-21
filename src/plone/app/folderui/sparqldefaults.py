8ad8d7bbab3cf8ce4d02c870d67d44d7abdc609c
Gerhard Weis <gerhard.weis@gmx.com>
2012-03-21 16:07:15 +1000

    query for file URIs instead of item URIs

--- src/plone/app/folderui/sparqldefaults.py
+++ src/plone/app/folderui/sparqldefaults.py
@@ -149,7 +149,8 @@ PARTICIPANT_GENDER_FACET = sparqlfacet(
     name=u'foaf_gender',
     title=u'Participant Gender',
     vocabquery='select distinct ?gender where { ?o <http://xmlns.com/foaf/0.1/gender> ?gender}',
-    resultquery='select distinct ?o where { ?o <http://www.language-archives.org/OLAC/1.1/speaker> ?s .'
+    resultquery='select distinct ?o where { ?item <http://ausnc.org.au/schemas/ausnc_md_model/document> ?o .'
+                                           '?item <http://www.language-archives.org/OLAC/1.1/speaker> ?s .'
                                            '?s <http://xmlns.com/foaf/0.1/gender> "%(value)s" }',
     # index=u'',
     description=u'Gender of participant',
