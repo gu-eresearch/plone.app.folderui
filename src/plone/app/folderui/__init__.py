578dac7426dbe3b7e6bdb7fe5e3c271bd079d56e
James Mills <j.mills@griffith.edu.au>
2013-10-08 14:46:32 +1000

    Code cleanup

--- src/plone/app/folderui/__init__.py
+++ src/plone/app/folderui/__init__.py
@@ -1,6 +1,7 @@
 from Products.CMFCore.DirectoryView import registerDirectory
 
+
 def initialize(context):
     """Initializer called when used as a Zope 2 product."""
-    registerDirectory('skins', globals())
 
+    registerDirectory("skins", globals())
