407141a56c412bcf67804a13434751d80c69a211
James Mills <j.mills@griffith.edu.au>
2013-10-08 14:41:47 +1000

    Code cleanup

--- src/plone/app/folderui/listing.py
+++ src/plone/app/folderui/listing.py
@@ -1,46 +1,59 @@
-from Products.CMFCore.interfaces._content import IFolderish
 from zope.interface import implements
 from zope.component import adapts, queryUtility, IFactory, ComponentLookupError
 
+from Products.CMFCore.interfaces._content import IFolderish
+
+
 from cache import PersistentRecordInvalidationSet
-from interfaces import (IQueryResults, IComposedQuery, IFacetSettings,
-    IQueryRunner, IFacetedListing, ISetCacheTools,)
+
+from interfaces import (
+    IComposedQuery, IFacetSettings,
+    IQueryRunner, IFacetedListing, ISetCacheTools,
+)
+
 from query import ComposedQuery
 from utils import dottedname, sitemanager_for
 
 
 class FacetedListing(object):
     """listing provider / (multi-)adapter for folder, query"""
-    
+
     implements(IFacetedListing)
     adapts(IFolderish)
-    
+
     def __init__(self, context, query=None, include_subfolders=False):
         if not IFolderish.providedBy(context):
             raise ValueError('context does not appear to be a folder')
+
         self.context = context
+
         if query and not IComposedQuery.providedBy(query):
             raise ValueError('query does not provide IComposedQuery')
         elif query is None:
             query = ComposedQuery()
+
         self.query = query
+
         sm = sitemanager_for(self.context)
+
         self.cachetools = sm.queryUtility(ISetCacheTools)
         if self.cachetools is None:
             raise ComponentLookupError('cannot find local set cache utility')
+
         self.include_subfolders = include_subfolders
-    
+
     def _result(self):
         if not hasattr(self, '_result_map'):
             factory = queryUtility(IFactory, (dottedname(IQueryRunner)))
             runner = factory(self.context)
             self._result_map = runner(self.query, self.include_subfolders)
-        return self._result_map #IQueryResults == iterable mapping
-    
+
+        return self._result_map  # IQueryResults == iterable mapping
+
     @property
     def result(self):
-        return self._result().values() #LazyMap of brains
-    
+        return self._result().values()  # LazyMap of brains
+
     @property
     def facets(self):
         if not hasattr(self, '_facets'):
@@ -49,21 +62,23 @@ class FacetedListing(object):
             if facets is None:
                 return []
             self._facets = facets.values()
+
         return self._facets
-    
+
     def _clear_invalidation(self, intersect):
         invalidated = self.cachetools.invalidated_records
         clear = set(invalidated) & set(intersect)
+
         if clear:
             s = set(self.cachetools.invalidated_records)
             for k in clear:
                 s.remove(k)
             self.cachetools.invalidated_records = \
                 PersistentRecordInvalidationSet(s)
-    
+
     def _counts_stale(self):
         return len(self.cachetools.invalidated_records) > 0
-    
+
     @property
     def counts(self):
         if not hasattr(self, '_counts') or self._counts_stale():
@@ -74,5 +89,5 @@ class FacetedListing(object):
                     key = '%s.%s' % (facet.name, filter.name)
                     self._counts[key] = filter.count(self.context, facet, intersect)
             self._clear_invalidation(intersect)
-        return self._counts
 
+        return self._counts
